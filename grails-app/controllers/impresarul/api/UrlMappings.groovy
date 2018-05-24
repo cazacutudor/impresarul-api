58130e47871b007f4d50e169f4290ce2c08b6bc5
Cazacu Tudor <laurentziu_lau97@yahoo.com>
2018-05-24 19:18:44 +0300

    UrlMappings - Update routes

--- grails-app/controllers/impresarul/api/UrlMappings.groovy
+++ grails-app/controllers/impresarul/api/UrlMappings.groovy
@@ -3,14 +3,15 @@ package impresarul.api
 class UrlMappings {
 
     static mappings = {
-        delete "/$controller/$id(.$format)?"(action: 'delete')
-        get "/$controller(.$format)?"(action: 'index')
-        get "/$controller/$id(.$format)?"(action: 'show')
-        post "/$controller(.$format)?"(action: 'save')
-        put "/$controller/$id(.$format)?"(action: 'update')
-        patch "/$controller/$id(.$format)?"(action: 'patch')
+
+        "/api/industries"(resources:'industry', excludes:['create', 'edit']) {
+            "/impresarios"(resources:'impresario', excludes:['create', 'edit']) {
+                "/talents"(resources:'talent', excludes:['create', 'edit'])
+            }
+        }
 
         "/"(view:"/index")
+        "/api"(view:"/index")
         "500"(view:'/error')
         "404"(view:'/notFound')
     }
