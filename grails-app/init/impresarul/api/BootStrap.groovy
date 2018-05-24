511826c249f0fcc4aa14a3b8f92a7c9d5b36c0cc
Cazacu Tudor <laurentziu_lau97@yahoo.com>
2018-05-24 19:18:25 +0300

    Add seeds data

--- grails-app/init/impresarul/api/BootStrap.groovy
+++ grails-app/init/impresarul/api/BootStrap.groovy
@@ -3,7 +3,21 @@ package impresarul.api
 class BootStrap {
 
     def init = { servletContext ->
-        new Impresario(name: "Impresarul Gigi").save()
+        new Industry(name: "Music", shortName: "music")
+            .addToImpresarios(
+                new Impresario(name: "Impresarul Gigi")
+                    .addToTalents(
+                        new Talent(name: "Talent Gigi")
+                )
+        ).save(flush:true, failOnError: true)
+
+        new Industry(name: "Movie", shortName: "movie")
+            .addToImpresarios(
+            new Impresario(name: "Impresarul Gigi")
+                .addToTalents(
+                new Talent(name: "Talent Gigi")
+            )
+        ).save(flush:true, failOnError: true)
     }
     def destroy = {
     }
