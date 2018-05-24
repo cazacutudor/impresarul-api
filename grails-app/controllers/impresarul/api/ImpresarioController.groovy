7c5978d30344621cd81a1190677b6a53bea37292
Cazacu Tudor <laurentziu_lau97@yahoo.com>
2018-05-24 19:13:25 +0300

    Update ImpresarioController

--- grails-app/controllers/impresarul/api/ImpresarioController.groovy
+++ grails-app/controllers/impresarul/api/ImpresarioController.groovy
@@ -8,4 +8,26 @@ class ImpresarioController extends RestfulController<Impresario> {
     ImpresarioController() {
         super(Impresario)
     }
+
+	def index() {
+		respond Impresario.findAllByIndustry(getIndustry())
+	}
+
+	def show(Long id) {
+		respond Impresario.findAllByIndustryAndId(getIndustry(), id)
+	}
+
+	def save() {
+		Impresario instance = new Impresario(industry: getIndustry())
+		bindData instance, request
+
+		saveResource instance
+
+		respond(instance, [status:201, formats: ['json']])
+	}
+
+	private Industry getIndustry() {
+		String industryId = params.industryId
+		return Industry.get(industryId)
+	}
 }
